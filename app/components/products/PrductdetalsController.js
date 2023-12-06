a82e580b0838ced1230deb33f15467f20e755b7e
Kiên Nguyễn Trung <kientrungng.2002@gmail.com>
2023-12-06 15:35:56 +0700

    6/12

--- app/components/products/PrductdetalsController.js
+++ app/components/products/PrductdetalsController.js
@@ -1,24 +1,25 @@
 const Product = require('../../model/ProductSchema');
 
-
 const getProductById = async (req, res) => {
     try {
-      const productId = req.params.id.toString();
-      const product = await Product.findOne({ _id: productId });
-        console.log('Product:', product); // In giá trị product vào console
-  
-        if (product) {
-            res.render('details/index',{ product: product});
-        } else {
-            res.status(404).send('Sản phẩm không tồn tại.');
+        const productId = req.params.id.toString();
+        const product = await Product.findOne({ _id: productId }).populate('category');
+
+        if (!product) {
+            return res.status(404).send('Sản phẩm không tồn tại.');
         }
+
+        const categoryId = product.category._id;
+
+        const relatedProducts = await Product.find({ category: categoryId, _id: { $ne: productId } }).limit(5);
+
+        res.render('details/index', { product, relatedProducts });
     } catch (error) {
         console.error(error);
         res.status(500).send('Đã xảy ra lỗi.');
     }
-  };
-  
+};
+
 module.exports = {
     getProductById,
-    
 };
