5e7e9c0ac459618b5aa5580ade8e35f4f64cd007
Giahyyyy <87819218+Giahyyyy@users.noreply.github.com>
2023-12-06 18:21:14 +0700

    update

--- app/components/Authentication/AuthenticationRoutes.js
+++ app/components/Authentication/AuthenticationRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/login', authenticationController.renderLoginPage);
 router.get('/register', authenticationController.renderRegisterPage);
 router.post('/register', authenticationController.registerUser)
+router.post('/login',authenticationController.loginUser)
 
 module.exports = router;
