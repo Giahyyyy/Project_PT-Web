432a550977589533507d392755efd506a82796a5
Giahyyyy <87819218+Giahyyyy@users.noreply.github.com>
2023-12-23 22:22:12 +0700

    register ajax

--- app/components/Authentication/AuthenticationRoutes.js
+++ app/components/Authentication/AuthenticationRoutes.js
@@ -4,13 +4,17 @@ const router = express.Router();
 
  
 
-router.get('/login',authenticationController.checkNotAuthenticated, authenticationController.renderLoginPage);
-router.get('/register',authenticationController.checkNotAuthenticated, authenticationController.renderRegisterPage);
+router.get('/login', authenticationController.renderLoginPage);
+router.get('/register', authenticationController.renderRegisterPage);
 
-router.post('/register',authenticationController.checkNotAuthenticated, authenticationController.registerUser);
-router.post('/login',authenticationController.checkNotAuthenticated, authenticationController.postLogin);
+router.post('/register', authenticationController.registerUser);
+router.post('/login', authenticationController.postLogin);
 
 router.delete('/logout',authenticationController.logout)
+
+
+// Định nghĩa một endpoint để xử lý yêu cầu kích hoạt
+//router.get('/activate/:activationCode', authenticationController.activateAccount);
 
 
 
